66878a037b08cc87b27d231e172990468b5364a3
user <user@user-PC>
2014-11-05 19:14:11 +0200

    Project rename

--- src/main/java/gr/teicm/se/ClosestOfferFinder/ClosestOfferFinderAppClient/Backend/Controller.java
+++ src/main/java/gr/teicm/se/ClosestOfferFinder/ClosestOfferFinderAppClient/Backend/Controller.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gr.teicm.se.ClosestOfferFinderApp.Backend;
+package gr.teicm.se.ClosestOfferFinder.ClosestOfferFinderAppClient.Backend;
 
 /**
  *
